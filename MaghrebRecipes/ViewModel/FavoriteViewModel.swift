ac6977c9b1b14098b1ce581d9044c081b434d93a
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-09 21:46:24 +0200

    changing insertion item for favorite

--- MaghrebRecipes/ViewModel/FavoriteViewModel.swift
+++ MaghrebRecipes/ViewModel/FavoriteViewModel.swift
@@ -11,11 +11,16 @@ import Foundation
 class FavoriteViewModel: ObservableObject {
     @Published var favoritesRecipes: [RecipeModel] = []
     @Published var isPresented: Bool = false
-    
+    @Published var isSheetOn: Bool = false
+
     func add(recipe: RecipeModel) {
-        self.favoritesRecipes.append(recipe)
+        self.favoritesRecipes.insert(recipe, at: 0)
     }
     
+    /// Move item on array from the view
+    /// - Parameters: Index for catch the item to int for move it at the right place
+    ///   - offsets: Index at the item is taken
+    ///   - destination: Number in the array where the item is moved
     func move(from offsets: IndexSet, to destination: Int) {
         self.favoritesRecipes.move(fromOffsets: offsets, toOffset: destination)
     }
@@ -27,6 +32,9 @@ class FavoriteViewModel: ObservableObject {
     func removeFavoriteRecipe(recipe: RecipeModel) {
         self.favoritesRecipes.removeAll { $0.id == recipe.id }
     }
+    
+    /// This function watch if the array of favoriteRecipes contains or not the recipe for delet or add to favorite conforming to RecipeModel.
+    /// - Parameter recipe: RecipeModel
     func addOrRemove(recipe: RecipeModel) {
         if favoritesRecipes.contains(recipe) {
             removeFavoriteRecipe(recipe: recipe)
