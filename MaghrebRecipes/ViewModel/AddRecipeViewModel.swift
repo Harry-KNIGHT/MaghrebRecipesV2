151d27db9d4948a876fa51fb6c2f5b9b20f44f9d
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-09 21:45:43 +0200

    Add recipe view model created

--- MaghrebRecipes/ViewModel/AddRecipeViewModel.swift
+++ MaghrebRecipes/ViewModel/AddRecipeViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+class AddRecipeViewModel: ObservableObject {
+    @Published var myRecipes: [RecipeModel] = []
+    @Published var isSheetOn = false
+    @Published var title: String = ""
+    @Published var price: String = ""
+    @Published var description: String = ""
+    @Published var photo: String = ""
+    @Published var recipeCategory: RecipeCategory = .entry
+    
+
+    
+    func createRecipe() {
+        let recipe = RecipeModel(title: title, price: Double(price) ?? 0, photo: photo, description: description, recipCategory: recipeCategory)
+        self.myRecipes.append(recipe)
+    }
+    
+    
+    func addRecipeButton() {
+            createRecipe()
+            title = ""
+            price = ""
+            description = ""
+            recipeCategory = .entry
+
+    }
+}
