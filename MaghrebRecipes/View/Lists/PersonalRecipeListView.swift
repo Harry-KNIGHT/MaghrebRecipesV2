1d671c99f674009e57ac33a844fe3c6b9559620e
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-09 21:42:17 +0200

    List for personnal recipes

--- MaghrebRecipes/View/Lists/PersonalRecipeListView.swift
+++ MaghrebRecipes/View/Lists/PersonalRecipeListView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct PersonalRecipeListView: View {
+    @EnvironmentObject var addRecipesVM: AddRecipeViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if !addRecipesVM.myRecipes.isEmpty {
+                    List {
+                        ForEach(addRecipesVM.myRecipes) { recipe in
+                            NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                                HStack(alignment: .center) {
+                                    RecipeImageView(recipe: recipe)
+                                    VStack(alignment: .leading, spacing: 10) {
+                                        Text(recipe.title)
+                                            .font(.headline)
+                                        Text(String(recipe.price) + "€")
+                                        Text(recipe.description)
+                                            .foregroundColor(.secondary)
+                                            .lineLimit(2)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }else {
+                    EmptyView(imageName: "exclamationmark.circle.fill", title: "Aucune recette crée").multilineTextAlignment(.center)
+                        .padding()
+                }
+            }
+            .navigationTitle("Mes recettes")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        addRecipesVM.isSheetOn.toggle()
+                    }, label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title2)
+                    }).sheet(isPresented: $addRecipesVM.isSheetOn) {
+                        AddRecipeForm()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct PersonalRecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        PersonalRecipeListView()
+        Group {
+            PersonalRecipeListView()
+                .environmentObject(AddRecipeViewModel())
+            PersonalRecipeListView()
+                .colorScheme(.dark)
+                .environmentObject(AddRecipeViewModel())
+        }
     }
 }
