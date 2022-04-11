3da43b1398147aa3a17cc9e20974eae2b0ec0feb
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-11 13:00:24 +0200

    Removing button for old order and their initialisation

--- MaghrebRecipes/View/Detail/RecipeDetailView.swift
+++ MaghrebRecipes/View/Detail/RecipeDetailView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
-    @EnvironmentObject var recipeVM: RecipeViewModel
     @EnvironmentObject var favoriteVM: FavoriteViewModel
     @EnvironmentObject var addRecipeVM: AddRecipeViewModel
     let recipe: RecipeModel
@@ -36,22 +35,12 @@ struct RecipeDetailView: View {
                     Text(recipe.description)
                 }
             }.listStyle(.inset)
-            
-            if !addRecipeVM.myRecipes.contains(recipe) {
-                Button("Commander") {
-                    // ajouter recette à la commande
-                    recipeVM.add(recipe: recipe)
-                }.padding(.bottom)
-                    .font(.title.bold())
-                    .buttonStyle(.borderedProminent)
-            }else {}
         }.modifier(Navigation(recipe: recipe))
     }
 }
 
 struct Navigation: ViewModifier {
     let recipe: RecipeModel
-    @EnvironmentObject var recipeVM: RecipeViewModel
     @EnvironmentObject var favoriteVM: FavoriteViewModel
     func body(content: Content) -> some View {
         content
@@ -69,7 +58,6 @@ struct RecipeDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             RecipeDetailView(recipe:  RecipeModel(title: "Maakouda", price: 7, photo: "maakouda", description: "La maaqouda, maqouda ou maakouda est un mets préparé et consommé en Algérie, au Maroc et en Tunisie, essentiellement pendant le mois de ramadan. Il s'agit d'une sorte de beignet de pommes de terre qui peut aussi se décliner avec du thon, de la viande hachée ou du fromage", recipCategory: .entry))
-                .environmentObject(RecipeViewModel())
                 .environmentObject(FavoriteViewModel())
                 .environmentObject(AddRecipeViewModel())
         }
