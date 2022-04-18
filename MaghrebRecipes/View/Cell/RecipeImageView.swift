56d8a48608b852527537f9174dedf3f0c60f4717
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-18 22:52:58 +0200

    Adding the image if recipe is vegetarian.

--- MaghrebRecipes/View/Cell/RecipeImageView.swift
+++ MaghrebRecipes/View/Cell/RecipeImageView.swift
@@ -12,18 +12,35 @@ struct RecipeImageView: View {
     let recipe: RecipeModel
     var width: CGFloat = 130
     var height: CGFloat = 80
+    var isShowingVege = true
     
+    
+    @Environment(\.colorScheme) var colorScheme
+    var sfFontSize: Font = .title2
+    var showingVegeWidth: CGFloat = 25
+    var showingVegeHeight: CGFloat = 25
     var body: some View {
         if !recipe.photo.isEmpty {
-        Image(recipe.photo).resizable()
-            .frame(width: width, height: height, alignment: .center)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
+            ZStack(alignment: .bottomTrailing) {
+                Image(recipe.photo).resizable()
+                    .frame(width: width, height: height, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                
+                if recipe.vegetarianRecipe {
+                Image(systemName: "leaf.circle.fill")
+                    .font(sfFontSize)
+                    .foregroundColor(.green)
+                    .background(colorScheme == .light ?
+                                Color.white.clipShape(Circle()) :  Color.black.clipShape(Circle())
+                    ).padding(-2)
+                }
+            }
         }else {
             ZStack {
-            RoundedRectangle(cornerRadius: 15)
-                .frame(width: width, height: height, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .foregroundColor(.secondary)
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: width, height: height, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .foregroundColor(.secondary)
                 Text("Maghreb Recipes")
                     .foregroundColor(.primary)
                     .font(.callout)
@@ -35,7 +52,9 @@ struct RecipeImageView: View {
 struct RecipeImageView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            RecipeImageView(recipe: RecipeModel(title: "Maakouda", photo: "maakouda", description: "La maaqouda, maqouda ou maakouda est un mets préparé et consommé en Algérie, au Maroc et en Tunisie, essentiellement pendant le mois de ramadan. Il s'agit d'une sorte de beignet de pommes de terre qui peut aussi se décliner avec du thon, de la viande hachée ou du fromage", ingredients: ["345G de fromage fondu"], recipCategory: RecipeCategory.entry, recipeDifficulty: .medium, recipeAveragePrice: .cheap, valueTimeCooking: 25, timeToCook: .minute))
+            RecipeImageView(recipe: RecipeModel(title: "Maakouda", photo: "maakouda", description: "La maaqouda, maqouda ou maakouda est un mets préparé et consommé en Algérie, au Maroc et en Tunisie, essentiellement pendant le mois de ramadan. Il s'agit d'une sorte de beignet de pommes de terre qui peut aussi se décliner avec du thon, de la viande hachée ou du fromage", ingredients: ["345G de fromage fondu"], recipCategory: RecipeCategory.entry, recipeDifficulty: .medium, recipeAveragePrice: .cheap, valueTimeCooking: 25, timeToCook: .minute, vegetarianRecipe: true))
+                .preferredColorScheme(.light)
+            RecipeImageView(recipe: RecipeModel(title: "Maakouda", photo: "maakouda", description: "La maaqouda, maqouda ou maakouda est un mets préparé et consommé en Algérie, au Maroc et en Tunisie, essentiellement pendant le mois de ramadan. Il s'agit d'une sorte de beignet de pommes de terre qui peut aussi se décliner avec du thon, de la viande hachée ou du fromage", ingredients: ["345G de fromage fondu"], recipCategory: RecipeCategory.entry, recipeDifficulty: .medium, recipeAveragePrice: .cheap, valueTimeCooking: 25, timeToCook: .minute, vegetarianRecipe: false))
                 .preferredColorScheme(.dark)
         }
     }
