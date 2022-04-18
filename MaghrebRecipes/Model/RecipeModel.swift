939b55c34ac7f29f2367c5a0a4629a85572af17e
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-18 15:17:19 +0200

    Perfectionning RecipeModel, added to it difficulty, average price time cooking and enums

--- MaghrebRecipes/Model/RecipeModel.swift
+++ MaghrebRecipes/Model/RecipeModel.swift
@@ -11,11 +11,15 @@ import Foundation
 struct RecipeModel: Identifiable, Equatable {
     var id = UUID()
     let title: String
-    let price: Double
     let photo: String
     let description: String
     let ingredients: [String]
+
     let recipCategory: RecipeCategory
+    let recipeDifficulty: RecipeDifficulty
+    let recipeAveragePrice: RecipeAveragePrice
+    let valueTimeCooking: Int
+    let timeToCook: TimeToCook
 }
 
 /// Category of recipes
@@ -23,4 +27,23 @@ enum RecipeCategory: String, CaseIterable {
     case entry = "Entrée"
     case dish = "Plat"
     case dessert = "Dessert"
+}
+
+
+enum RecipeDifficulty: String, CaseIterable {
+    case easy = "Facile"
+    case medium = "Moyenne"
+    case hard = "Avancée"
+}
+
+enum RecipeAveragePrice: String, CaseIterable {
+    case cheap = "Économique"
+    case medium = "Moyen"
+    case expensive = "Cher"
+}
+
+enum TimeToCook: String, CaseIterable {
+    case minute = "Minutes"
+    case hour = "Heures"
+    case day = "Jours"
 }
