118d8cf592f4b07e0e1a96363c8e394b2c367297
Harry-KNIGHT <eliott.knight@icloud.com>
2022-04-20 13:51:26 +0200

    Go to form view modified

--- MaghrebRecipes/View/Cell/CreateRecipeButtonCell.swift
+++ MaghrebRecipes/View/Cell/CreateRecipeButtonCell.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct CreateRecipeButtonCell: View {
-    @State private var isShowingSheet: Bool = false
+    @State private var isActive = false
     var body: some View {
+        
+        NavigationLink(destination: AddRecipeForm(), isActive: $isActive) {
         Button(action: {
-            isShowingSheet = true
+            self.isActive = true
         }, label: {
             Label("Créer une recette", systemImage: "plus.circle.fill")
         })
-        .buttonPersonnalStyle()
-        .sheet(isPresented: $isShowingSheet) {
-            AddRecipeForm()
+        .buttonPersonnalStyle(.title)
         }
     }
 }
@@ -25,21 +25,5 @@ struct CreateRecipeButtonCell: View {
 struct CreateRecipeButtonCell_Previews: PreviewProvider {
     static var previews: some View {
         CreateRecipeButtonCell()
-    }
-}
-
-
-struct CreateRecipeButtonCellExtractedView: View {
-    @State private var isShowingSheet: Bool = false
-    var body: some View {
-        Button(action: {
-            isShowingSheet = true
-        }, label: {
-            Label("Créer une recette", systemImage: "plus.circle.fill")
-        })
-        .buttonPersonnalStyle(.title)
-        .sheet(isPresented: $isShowingSheet) {
-            AddRecipeForm()
-        }
     }
 }
